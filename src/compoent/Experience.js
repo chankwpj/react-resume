941b041b98d1005c0a17dd6234a10781239fd02c
Kai Chan <chankw.jh@gmail.com>
2020-05-08 22:10:00 +0100

    fix experience item date

--- src/compoent/Experience.js
+++ src/compoent/Experience.js
@@ -5,13 +5,25 @@ class Experience extends React.Component {
     render() {
         // console.log(this.props.item);
         const exp = this.props.item;
+        
+        let dateBlock;
+        if (exp.from.length === 0) {
+            dateBlock = '';
+        } else {
+            if (exp.to.length ===0) {
+                dateBlock = exp.from;
+            } else {
+                dateBlock = exp.from + ' - ' + exp.to;
+            }
+        }
+
         return (
             <div className='expItem'>
                 {exp.companyName.length !== 0 &&
                     <p className='employer'>{exp.companyName} - {exp.jobTitle}</p>
                 }
                 {exp.from.length !== 0 && 
-                    <p>{exp.from} - {exp.to} &emsp;&emsp;&emsp;&emsp; {exp.location}</p>
+                    <p>{dateBlock} &emsp;&emsp;&emsp;&emsp; {exp.location}</p>
                 }
                 <p className='team'>{exp.team}</p>
                 {exp.descriptions.map((desc, index) => {
