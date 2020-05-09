2f6407cf50ac2630115ee6df24ff202e65dd2c60
Kai Chan <chankw.jh@gmail.com>
2020-05-10 00:01:16 +0100

    verify passing props in test

--- src/compoent/awards.test.js
+++ src/compoent/awards.test.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import DummyAward from './Award';
+import Award from './Award';
 import Awards from './Awards';
 
 jest.mock("./Award", () => {
     return function DummyAward(props) {
-        return (
-            <p key={props.key}>{props.awardItem}</p>
-        );
+        return (<div></div>);
     };
 });
 
-it("renders awards items", () => {
+it("renders 2 award items", () => {
     const awardItems = ["dummy1", "dummy1"];
     const wrapper = shallow((<Awards awardItems={awardItems}/>));
-    expect(wrapper.find(DummyAward)).to.have.lengthOf(2);
+    expect(wrapper.find(Award)).to.have.lengthOf(2);
+    expect(wrapper.find(Award).at(0).props()).to.eql({"awardItem": awardItems[0]});
+    expect(wrapper.find(Award).at(1).props()).to.eql({"awardItem": awardItems[1]});
 });
