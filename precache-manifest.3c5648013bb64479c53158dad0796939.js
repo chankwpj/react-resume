fd94c25b498357328720d960282312c8c6b972ed
Kai Chan <chankw.jh@gmail.com>
2020-06-26 22:24:06 +0100

    Updates

---
+++ precache-manifest.3c5648013bb64479c53158dad0796939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd45263230556de959d858fb416e5144",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "8b46963736ff518297c1",
+    "url": "/react-resume/static/css/main.7b8e1f00.chunk.css"
+  },
+  {
+    "revision": "fb135306b72375d8a792",
+    "url": "/react-resume/static/js/2.8864f8e3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-resume/static/js/2.8864f8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b46963736ff518297c1",
+    "url": "/react-resume/static/js/main.69d693e0.chunk.js"
+  },
+  {
+    "revision": "b0a3f5ec7736481f1bae",
+    "url": "/react-resume/static/js/runtime-main.b1454228.js"
+  }
+]);
